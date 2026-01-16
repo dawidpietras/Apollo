bef72534b6385ae9586a5827b5008ba7c04ce488
Dawid Pietras <dawid.pietras.91@gmail.com>
2026-01-16 14:10:16 +0100

    first approach to double chat

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 chatbot_page = st.Page("pages//chatbot.py", title="Chatbot", icon="💬")
 shopping_list_page = st.Page("pages//shopping_list.py", title="Shopping List", icon="🛒")
 record_page = st.Page("pages//recorder_page.py", title="Recorder", icon="🎤")
+two_chats_page = st.Page("pages//two_chats.py", title="Two chats", icon="💬")
 
 pg = st.navigation([chatbot_page, shopping_list_page, record_page])
 st.set_page_config(page_title="Dietetyk AI", page_icon=":material/edit:")
