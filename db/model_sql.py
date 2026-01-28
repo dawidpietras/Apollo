85c0202a403d9489b1274fe3d5693c98d9c774f3
Dawid Pietras <dawid.pietras.91@gmail.com>
2026-01-28 08:37:48 +0100

    sqlmodels defined

--- db/model_sql.py
+++ db/model_sql.py
@@ -1,8 +1,35 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine
 
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     secret_name: str
     age: int | None = None
-     
+
+sqlite_file_name = "database.db"
+
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def create_heroses():
+    hero_1 = Hero(name="Iron Man", secret_name="Tony Stark")
+    hero_2 = Hero(name="Kapitan Ameryka", secret_name="Steve Rogers")
+
+
+    with Session(engine) as session:
+
+        session.add(hero_1)
+        session.add(hero_2)
+        session.commit()
+    
+
+def main():
+    create_db_and_tables()
+    create_heroses()
+
+if __name__ == "__main__":
+    main()
---
+++ models/models.py
@@ -0,0 +1,34 @@
+from sqlmodel import SQLModel, Field
+from enum import Enum
+
+class Priority(str, Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+class ShoppingItem(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(..., description="Name of the ingredient in polish language")
+    quantity: int = Field(None,
+                          description="""Item quantity declared in pieces
+                          (szt in polish) or in grams depends on user input.
+                          Use polisz szt or just g for grams. This field can be empty
+                          because user can just say 'bananas' without defining quantity.
+                          Quantity unit is not included here just number.""")
+    unit: str = Field(None,
+                      description="""Quantity unit declared by user. Use szt for slices or
+                      units and grams (g) for weight units.""")
+    bought: bool = Field(False,
+                         description="""If no info is given from user, assume that False is
+                         the right value""")
+
+
+class ToDoTask(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(..., description="Title of the task taht will be visible in to-do list")
+    priority: Priority = Field(Priority.MEDIUM,
+                               description="""Declared priority of the task. If user declared
+                               similar level, for example moderate instead of medium try to
+                               adjust it to allowed values.""")
+    assigned: str = Field(None, description="""If user assigned someone to mentioned task
+                          then write here the name, full name or nick.""")
